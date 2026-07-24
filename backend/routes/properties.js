421d610b75738b690d1038ec79af5962bb1ab02d
Muhammad Abdullah Bhatti <abdullah009bhatti@gmail.com>
2026-07-24 19:41:05 +0500

    fix: Remove invalid authMiddleware import causing server crash

--- backend/routes/properties.js
+++ backend/routes/properties.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Property = require('../models/Property');
 const PaymentInstallment = require('../models/PaymentInstallment');
-const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
